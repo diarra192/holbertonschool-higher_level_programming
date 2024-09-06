c5772662a8791d0414349fe81538bfbe4e888793
diarra192 <dboureima192@gmail.com>
2024-09-06 14:20:34 +0200

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    print(f"{i:02}", end=", " if i < 99 else "\n")
+
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
