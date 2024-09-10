10a1f66b9af296827997410a4b9f5f1f60c79ed4
Nah <149107477+diarra192@users.noreply.github.com>
2024-09-10 09:50:59 +0200

    Update 2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, elemet):
-    if idx < 0 or idx >= len(my_list):
-        return my_list #ne modifie rien et returne la liste original
-    my_list[idx] = element 
-    return my_list
-        
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
