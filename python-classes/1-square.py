a06cb3e83ecef088b7bd6e665bbd244a26af30ac
Nah <149107477+diarra192@users.noreply.github.com>
2024-09-20 18:48:45 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-
 class Square:
-    """
-    class square that has attributes:
-        size
+    """Class Square that defines a square object
     """
     def __init__(self, size):
-        """
-        the initialization function for the square class
+        """Initialize method that stores the size of the square
+
+        Args:
+            param1 (int): size of the square
         """
         self.__size = size
