7a276923ab1da5a4de46552402e03308f4b86168
Nah <149107477+diarra192@users.noreply.github.com>
2024-09-20 18:56:02 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+
 class Square:
-    """Class Square that defines a square object
-    """
+    """Class Square that defines a square object."""
+
     def __init__(self, size):
-        """Initialize method that stores the size of the square
+        """Initialize method that stores the size of the square.
 
         Args:
-            param1 (int): size of the square
+            size (int): Size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
