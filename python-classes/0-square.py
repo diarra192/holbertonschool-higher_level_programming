407f01be397ad0cdef6970ff041701fdc65d5f85
diarra192 <dboureima192@gmail.com>
2024-09-20 18:38:00 +0200

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
+
 
 
 class Square:
@@ -7,8 +7,7 @@ class Square:
     empty class for Square
     """
     pass
-=======
+
 class Square:
     """une classe vide qui definit un carré """
     pass
->>>>>>> c26540128d56d67f66d729d2541b3b5133035c0f
