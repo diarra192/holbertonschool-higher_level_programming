4c7e67c38cb70bdb80e62f71f335a2fc13c2fb90
diarra192 <dboureima192@gmail.com>
2024-09-23 15:08:58 +0200

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+"""Class square that has attributes"""
 
 class Square:
     """
@@ -7,14 +8,17 @@ class Square:
         size
     some attributes are protected from input.
     """
+
     def __init__(self, size=0):
         """
         the initialization function for the square class
         checks for input errors for size
         """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
