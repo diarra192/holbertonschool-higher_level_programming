778d72f53e1b57e363583fadddd8ed55d1f742f0
diarra192 <dboureima192@gmail.com>
2024-09-23 15:19:42 +0200

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+""" class square that has attributes """
+
 
 class Square:
     """
@@ -7,10 +9,12 @@ class Square:
         size
     some attributes are protected from input.
     """
+
     def __init__(self, size=0):
         """
         initialization function for our square clasee
         """
+
         if self.__validate_size(size):
             self.__size = size
 
@@ -19,6 +23,7 @@ class Square:
         """
         getter for the size property
         """
+
         return self.__size
 
     @size.setter
@@ -26,6 +31,7 @@ class Square:
         """
         setter for the size property
         """
+
         if self.__validate_size(value):
             self.__size = value
 
@@ -33,12 +39,14 @@ class Square:
         """
         calculates the area of the square
         """
+
         return self.__size ** 2
 
     def my_print(self):
         """
         prints the square using '#' characters
         """
+
         i = 0
         for i in range(0, self.__size):
             j = 0
@@ -50,6 +58,7 @@ class Square:
         """
         validates the size, checking for errors
         """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -57,3 +66,4 @@ class Square:
         else:
             return True
         return False
+
