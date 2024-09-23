21aee58b812a40975e05084dacae7900de8c362d
diarra192 <dboureima192@gmail.com>
2024-09-23 15:06:25 +0200

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""class defin a square"""
+
+
 class Square:
     """Class Square that defines a square object."""
 
@@ -13,8 +16,10 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
