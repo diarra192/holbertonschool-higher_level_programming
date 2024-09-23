1b016d9b5191f8f160b5bd5a5674c2adb9063011
diarra192 <dboureima192@gmail.com>
2024-09-23 15:24:02 +0200

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 """ class that has define """
-"""class that has define """
-"""class that has define"""
 
 
 Square = __import__('4-square').Square
+
 """class thas define attribut"""
 
 my_square = Square(89)
