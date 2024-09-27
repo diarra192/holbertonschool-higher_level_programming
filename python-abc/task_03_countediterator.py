9284dc0023969e7f38664b2bfd3a425babee2890
diarra192 <dboureima192@gmail.com>
2024-09-27 16:53:38 +0200

    python

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
-from task_03_countediterator import CountedIterator
+"""
+Extends Python's built-in iterator to keep a count of the number of items
+iterated over.
+"""
 
-data = [1, 2, 3, 4]
-counted_iter = CountedIterator(data)
 
-try:
-    while True:
-        item = next(counted_iter)
-        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
-except StopIteration:
-    print("No more items.")
+class CountedIterator:
+    """
+    Iterator that counts the number of items iterated over.
+    """
+
+    def __init__(self, iterable):
+        """
+        Initialize the iterator and counter.
+
+        Args:
+            iterable: An iterable object to iterate over.
+        """
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        """
+        Return the next item from the iterator and increment the count.
+
+        Raises:
+            StopIteration: If there are no more items to iterate.
+        """
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise
+
+    def get_count(self):
+        """
+        Return the number of items that have been iterated over.
+
+        Returns:
+            int: The count of items iterated.
+        """
+        return self.count
