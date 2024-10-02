37ebae4a39624e5a7d5b98c6f9946b28bd578cd8
Nah <149107477+diarra192@users.noreply.github.com>
2024-10-02 09:18:13 +0200

    Update 0-read_file.py
    read file with python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def read_file(filename= "") :
-    with open(filename, 'r', encoding= 'utf-8') as file:
-        print (file.read())
+"""read_file
+"""
+
+
+def read_file(filename=""):
+    """Takes in str filename to read it
+    """
+
+    with open(filename, encoding="utf-8") as readFile:
+        print(readFile.read(), end='')
